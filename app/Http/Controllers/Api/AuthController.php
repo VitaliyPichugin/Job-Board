980ff80a51c67783c6a73d42ca92da619e1a200c
VitaliyPichugin <vexel1992@gmail.com>
2022-10-15 14:13:12 +0300

    Add JWT auth

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('api', ['except' => ['login', 'register']]);
+    }
+
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+                               'email' => 'required',
+                               'password' => 'required',
+                           ]);
+
+        $credentials = request(['email', 'password']);
+
+        if (!$token = auth('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Email or Password Invalid'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Register a User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                                   'name' => ['required', 'string', 'max:255'],
+                                   'email' => [
+                                       'required',
+                                       'string',
+                                       'email',
+                                       'max:255',
+                                       'unique:users'
+                                   ],
+                                   'password' => ['required', 'string', 'confirmed'],
+                               ]);
+
+
+            $data = array();
+            $data['name'] = $request->name;
+            $data['email'] = $request->email;
+            $data['password'] = Hash::make($request->password);
+            User::create($data);
+
+            return $this->login($request);
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage());
+        }
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth('api')->logout();
+        return response()->json(['message' => 'User successfully signed out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth('api')->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+                                    'access_token' => $token,
+                                    'token_type' => 'bearer',
+                                    'expires_in' => auth('api')->factory()->getTTL() * 60,
+                                    'user' => auth('api')->user(),
+                                ]);
+    }
+}
