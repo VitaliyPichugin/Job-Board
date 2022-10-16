5a339e55c313bb71f492c786d916727aeb6a4b95
VitaliyPichugin <vexel1992@gmail.com>
2022-10-16 03:08:29 +0300

    fix conf

--- app/Services/CoinService.php
+++ app/Services/CoinService.php
@@ -43,9 +43,10 @@ class CoinService
     {
         $users = User::all();
         $new_coins = config('global.add_coins');
+        $accumulate_coins = config('global.accumulate_coins');
         foreach ($users as $user) {
             $user_coins = $user->coin;
-            $user_coins = $user->coin <= 5 ? $user_coins : 5;
+            $user_coins = ($user->coin <= $accumulate_coins) ? $user_coins : $accumulate_coins;
             $user->coin = $user_coins + $new_coins;//accumulated up to 5 coins
             if ($user->save()) {
                 Mail::raw("$new_coins coins credited. On your account $user->coin coins", function ($mail) use ($user) {
--- config/global.php
+++ config/global.php
@@ -2,6 +2,7 @@
 
 return [
     'limit_send_job' => 2,
+    'accumulate_coins' => 5,
     'period_send_job' => 3600 * 24,
     'send_response' => 1,
     'post_job' => 2,
